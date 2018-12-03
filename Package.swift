bb9b97ca573f8217d911879a8a6f46e923531e1b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-03 10:57:52 -0600

    Installed skelpo/Countries package

--- Package.swift
+++ Package.swift
@@ -8,10 +8,11 @@ let package = Package(
         .library(name: "TaxJar", targets: ["TaxJar"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/skelpo/Countries.git", from: "0.9.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0")
     ],
     targets: [
-        .target(name: "TaxJar", dependencies: ["Vapor"]),
+        .target(name: "TaxJar", dependencies: ["Vapor", "Countries"]),
         .testTarget(name: "TaxJarTests", dependencies: ["TaxJar"]),
     ]
 )
