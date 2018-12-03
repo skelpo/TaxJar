9b63eeb51500c32b5b0cf1b66c86623bea78540c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-03 11:09:52 -0600

    Added .id property to Address model

--- Sources/TaxJar/Models/Address.swift
+++ Sources/TaxJar/Models/Address.swift
@@ -4,6 +4,9 @@ import Vapor
 /// An address that can be used for things such as the location an order is coming from or going to.
 public struct Address: Content, Equatable {
     
+    /// Unique identifier of the given nexus address.
+    public var id: String?
+    
     /// Two-letter ISO country code of the country.
     public var country: Country
     
@@ -22,12 +25,13 @@ public struct Address: Content, Equatable {
     /// Creates a new `Address` instance.
     ///
     /// - Parameters:
+    ///   - id: Unique identifier of the given nexus address.
     ///   - country: Two-letter ISO country code of the country.
     ///   - zip: Postal code (5-Digit ZIP or ZIP+4).
     ///   - state: Two-letter ISO state code.
     ///   - city: City name.
     ///   - street: Street address.
-    public init(country: Country, zip: String, state: Province, city: String, street: String) {
+    public init(id: String? = nil, country: Country, zip: String, state: Province, city: String, street: String) {
         self.country = country
         self.zip = zip
         self.state = state
