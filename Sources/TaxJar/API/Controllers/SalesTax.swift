253d251cee464a45924eb31e269cd527a4690dd2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-04 13:32:55 -0600

    Created SalesTax service

---
+++ Sources/TaxJar/API/Controllers/SalesTax.swift
@@ -0,0 +1,29 @@
+import Vapor
+
+public final class SalesTax: ServiceType {
+    public static func makeService(for worker: Container) throws -> SalesTax {
+        return self.init(container: worker)
+    }
+    
+    public let container: Container
+    
+    public init(container: Container) {
+        self.container = container
+    }
+    
+    public func tax(for order: Tax.CalculateRequest) -> Future<Tax> {
+        do {
+            let key = try self.container.make(Configuration.self).key
+            
+            let body = try JSONEncoder().encode(order)
+            let http = HTTPRequest(method: .POST, url: "https://api.taxjar.com/v2/taxes", headers: ["Authorization": "Bearer " + key], body: body)
+            let request = Request(http: http, using: self.container)
+            
+            let response = try self.container.client().send(request)
+            return response.flatMap { try $0.content.decode(Tax.self) }
+            
+        } catch let error {
+            return self.container.future(error: error)
+        }
+    }
+}
